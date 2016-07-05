7b05bfa2f0a0ef865422e6d63054b085bef7401e
Gustavo Ferreira <gustavogardel@gmai.com>
2016-07-05 16:00:32 -0700

    added new function to redraw the board

--- Rearrange_Puzzle_Game/JS/script.js
+++ Rearrange_Puzzle_Game/JS/script.js
@@ -1,14 +1,26 @@
 //
 function restart(){
-m = 0;
-for(i=0; i < oCells.length; i++){
-oCells[i].innerHTML = '';
+  m = 0;
+  for(i=0; i < oCells.length; i++){
+    oCells[i].innerHTML = '';
+  }
+
+  //
+  numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  //
+  numbers.shuffle();
+  for(i=0; i < numbers.length; i++){
+    oCells[i].innerHTML = numbers[i];
+  }
+  reDrawBoard(numbers.length);
 }
 
-//
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-//
-numbers.shuffle();
-for(i=0; i < numbers.length; i++){
-oCells[i].innerHTML = numbers[i];
+
+
+
+
+function reDrawBoard(num){
+  for(var i = 0; i < oCells.length; i++){
+    oCells[i].onclick = oCells[i].length.className = oCells[i].title = "";
+  }
 }
